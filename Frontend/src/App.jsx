2682ae61eece6d0d4dbc9ffc63ba37b0707a6dd7
Yawer091 <yaweratique90@gmail.com>
2024-03-26 15:22:55 +0530

    Material Ui added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Button from "@mui/material/Button";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Button variant="contained">Hello world</Button>
+    </>
+  );
 }
 
 export default App;
