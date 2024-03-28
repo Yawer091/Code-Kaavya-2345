a9990a7643459df10b0c524055819966ef025d80
BhaweshPandey-03 <bhaweshpandey0310@gmail.com>
2024-03-28 22:05:58 +0530

    routes, env : created

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express.json();
+const cors = require("cors");
+const PORT = process.env.PORT || 8080;
+
 app.listen(8080, () => {
   console.log("Runnig at Port 8080");
 });
