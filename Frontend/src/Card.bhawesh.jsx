1bf595d8116f77b124618b08c2c43a7e1dcdb8dc
BhaweshPandey-03 <bhaweshpandey0310@gmail.com>
2024-03-30 13:45:56 +0530

    css added

--- Frontend/src/Card.bhawesh.jsx
+++ Frontend/src/Card.bhawesh.jsx
@@ -1,12 +1,21 @@
-export const CardBhawesh = (item) => {
-    
-    return <>
+import PropTypes from 'prop-types';
+
+export const CardBhawesh = ({ item }) => {
+    return (
         <div className="card-b-1">
-        <div>
-            <img src= {item.image}/>
+            <div>
+                <img src={item.img} alt={item.name} />
+            </div>
+            <h5>{item.info}</h5>
+            <p>{item.name}</p>
         </div>
-        <p>{item.type}</p>
-        <p>{item.name}</p>
-        </div>
-    </>
-}
+    );
+};
+
+CardBhawesh.propTypes = {
+    item: PropTypes.shape({
+        img: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        info: PropTypes.string.isRequired,
+    }).isRequired,
+};
