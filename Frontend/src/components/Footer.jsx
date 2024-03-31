7ec816e7f5f96b95daa38c4d137d27d620da8188
Yawer091 <yaweratique90@gmail.com>
2024-03-31 14:27:46 +0530

    Frontend

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,159 +1,123 @@
-import React from "react";
+import {
+  Box,
+  Divider,
+  Flex,
+  Heading,
+  Icon,
+  Text,
+  Link,
+} from "@chakra-ui/react";
+import {
+  FaFacebook,
+  FaTwitter,
+  FaEnvelope,
+  FaPinterest,
+  FaLinkedin,
+} from "react-icons/fa";
+import { useLocation } from "react-router-dom";
 
-const Footer = () => (
-  <footer className="bg-[#002684] text-white p-[50px]">
-    <div className="flex justify-between px-4 text-[#a1bce6] w-[80%] m-auto">
-      <div className="footer-column">
-        <h3 className="text-lg font-semibold">On the Menu</h3>
-        <ul className="mt-2">
-          <li>
-            <a href="#" className="hover:text-white">
-              Pricing
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-white">
-              Our Vision
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-white">
-              Market
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-white">
-              Gift Cards
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-white">
-              Blog
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-white">
-              Cookbook
-            </a>
-          </li>
-        </ul>
-      </div>
+function Footer() {
 
-      <div className="footer-column">
-        <h3 className="text-lg font-semibold">Suppliers</h3>
-        <ul className="mt-2">
-          <li>
-            <a href="#" className="hover:text-white">
-              Affiliates
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-white">
-              Supply Chains Act
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-white">
-              Food Safety
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-white">
-              Career
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-white">
-              Press
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-white">
-              Our Team
-            </a>
-          </li>
-        </ul>
-      </div>
+  const location = useLocation();
 
-      <div className="footer-column">
-        <h3 className="text-lg font-semibold">Military & Veterans</h3>
-        <ul className="mt-2">
-          <li>
-            <a href="#" className="hover:text-white">
-              Students
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-white">
-              Graduates
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-white">
-              Teachers
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-white">
-              Seniors (+55)
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-white">
-              Medical Staff
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-white">
-              First Responders
-            </a>
-          </li>
-        </ul>
-      </div>
+  if(location.pathname === "/admin") {
+    return
+  }
 
-      <div className="footer-column">
-        <h3 className="text-lg font-semibold text-white">Customer Support</h3>
-        <ul className="mt-2 ">
-          <li>
-            <a href="#" className="hover:text-white">
-              Help Center & FAQ
-            </a>
-          </li>
-          <li>
-            <a href="mailto:contact@HomeChef.com" className="hover:text-white">
-              contact@HomeChef.com
-            </a>
-          </li>
-          <li>
-            <a href="tel:(646) 891-4349" className="hover:text-white">
-              (646) 891-4349
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-
-    <div className="text-center mt-[30px]">
-      <p className="text-white">© Home Chef, LLC 2024</p>
-      <ul className="mt-2">
-        <li className="inline-block mr-2">
-          <a href="#" className="hover:text-white">
-            Ad Preferences
-          </a>
-        </li>
-        <li className="inline-block mr-2">
-          <a href="#" className="hover:text-white">
-            Privacy
-          </a>
-        </li>
-        <li className="inline-block">
-          <a href="#" className="hover:text-white">
-            Terms
-          </a>
-        </li>
-      </ul>
-    </div>
-  </footer>
-);
+  return (
+    <>
+      <Divider orientation="horizontal" marginy="2rem" borderRadius="full" />
+      <Box width="min(80rem,100%)" mx="auto" py={10} color="text" px={4}>
+        <Text
+          fontFamily={"Kaushan Script"}
+          fontSize="2xl"
+          fontWeight="bold"
+          marginBottom="2rem"
+        >
+          Recipe
+          <Text display="inline" color="primary.500">
+            Hub
+          </Text>
+        </Text>
+        <Flex
+          wrap={'wrap'}
+          gap={4}
+          justifyContent="space-between"
+        >
+          <Box>
+            <Heading as="h6" size="MD" marginBottom="16px">
+              ABOUT NCG
+            </Heading>
+            <Text>Shop</Text>
+            <Text>About</Text>
+            <Text>Work with me</Text>
+            <Text>Contact</Text>
+          </Box>
+          <Box p={0} mr={4}>
+            <Heading as="h6" size="MD" marginBottom="16px">
+              EXPLORE
+            </Heading>
+            <Text>Recipes</Text>
+            <Text>Fitness</Text>
+            <Text>Healthy living</Text>
+            <Text>Blogs</Text>
+          </Box>
+          <Box>
+            <Heading as="h6" size="md" marginBottom="16px">
+              Connect
+            </Heading>
+            <Flex alignItems="center">
+              <Link href="#">
+                <Icon
+                  as={FaFacebook}
+                  boxSize={6}
+                  marginRight="16px"
+                  transition="0.2s ease-in"
+                  _hover={{ color: "primary.500" }}
+                />
+              </Link>
+              <Link href="#">
+                <Icon
+                  as={FaTwitter}
+                  transition="0.2s ease-in"
+                  _hover={{ color: "primary.500" }}
+                  boxSize={6}
+                  marginRight="16px"
+                />
+              </Link>
+              <Link href="#">
+                <Icon
+                  as={FaEnvelope}
+                  transition="0.2s ease-in"
+                  _hover={{ color: "primary.500" }}
+                  boxSize={6}
+                  marginRight="16px"
+                />
+              </Link>
+              <Link href="#">
+                <Icon
+                  as={FaPinterest}
+                  transition="0.2s ease-in"
+                  _hover={{ color: "primary.500" }}
+                  boxSize={6}
+                  marginRight="16px"
+                />
+              </Link>
+              <Link href="#">
+                <Icon
+                  as={FaLinkedin}
+                  transition="0.2s ease-in"
+                  _hover={{ color: "primary.500" }}
+                  boxSize={6}
+                  marginRight="16px"
+                />
+              </Link>
+            </Flex>
+          </Box>
+        </Flex>
+      </Box>
+    </>
+  );
+}
 
 export default Footer;
